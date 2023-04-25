658c59a8722d2ffb18d8e96a78f9d55b8b20db9f
iasonotk <iasonotk@gmail.com>
2023-04-25 15:28:28 +0400

    create menu elements

---
+++ app/src/pages/AboutPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>მეს შესახებ</h1>
+      <p>გამარჯობა მე ჯონ დო ვარ</p>
+      <p>ეს გვერდი იდეაში განკუთვნილია იმისთვის რომ კრეატიული კონტენტი გამოჩნდეს აქ</p>
+    </div>
+  );
+}
+
+export default AboutPage;
