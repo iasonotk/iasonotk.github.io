d2d3fc9652d62f5a6b428543c45bd8f3cce5cbf3
iasonotk <iasonotk@gmail.com>
2023-04-25 15:25:06 +0400

    create menu elements

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import Menu from './components/Menu';
+// import HomePage from './pages/home_page/HomePage';
+import AboutPage from './pages/AboutPage';
+import Menu from './components/menu/Menu';
+// import BlogPage from './pages/blog_pageBlogPage';
+// import PortfolioPage from './pages/portfolio_page/PortfolioPage';
+// import ContactPage from './pages/ContactPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Menu />
+      </div>
+      <Routes>
+        {/* <Menu /> */}
+        {/* <Route exact path="/" component={HomePage} /> */}
+        <Route path="/about" element={<AboutPage />} />
+        {/* <Route path="/blog" component={BlogPage} />
+        <Route path="/portfolio" component={PortfolioPage} />
+        <Route path="/contact" component={ContactPage} /> */}
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
